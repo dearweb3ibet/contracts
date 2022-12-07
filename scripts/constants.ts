67ff02c001c864647671e26399eb9477ec946344
kiv1n <aloha@kiv1n.ru>
2022-12-07 21:33:32 +0300

    Move deployd contract addresses to constants file

---
+++ scripts/constants.ts
@@ -0,0 +1,17 @@
+export const deployedContracts: {
+  [key: string]: {
+    betChecker: string;
+    contest: string;
+    usage: string;
+    bet: string;
+    bio: string;
+  };
+} = {
+  mumbai: {
+    betChecker: "0xE78Ec547bdE5697c1Dd2B32524c9a51F4385CC08",
+    contest: "0xD09601e5a806c177483cA0F6deBf47f9D6B30cE7",
+    usage: "0xD67c41DE6FE17e13060A7c7534aFCD4CD5c4afcB",
+    bet: "0x9E5fA82C9a529c1d7Bd59D3D7AfF597D2948f744",
+    bio: "0x752ab4DDf258eec8857a9115fAed1E3afE1Abbe5",
+  },
+};
