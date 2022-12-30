c87880fe2297a6e72d621d0018c5b9a5f964b7f9
kiv1n <aloha@kiv1n.ru>
2022-12-30 21:10:28 +0300

    Add more symbols of bet checker contract for deploying

--- scripts/helpers/constants.ts
+++ scripts/helpers/constants.ts
@@ -82,8 +82,12 @@ export const deployedContractsData: {
 } = {
   mumbai: {
     betChecker: {
-      feedSymbols: ["ETHUSD"],
-      feedAddresses: ["0x0715A7794a1dc8e42615F059dD6e406A6594651A"],
+      feedSymbols: ["ETHUSD", "BTCUSD", "MATICUSD"],
+      feedAddresses: [
+        "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+        "0x007A22900a3B98143368Bd5906f8E17e9867581b",
+        "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada",
+      ],
     },
     bet: {
       contestFeePercent: 15,
